882f1ade8de3e552abf03da8ed550e4f81836652
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-12 17:54:24 +0700

    Update Supabase URL in configuration file

--- assets/js/supabase-confog.js
+++ assets/js/supabase-confog.js
@@ -3,7 +3,7 @@
 // Pastikan menggunakan CDN Supabase yang ringan untuk Vanilla JS
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = "YOUR_SUPABASE_URL"; 
+const SUPABASE_URL = "https://junramfhdgabmytoaazh.supabase.co/rest/v1/"; 
 const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
 
 // Inisialisasi client Supabase secara Singleton (Satu koneksi global)
