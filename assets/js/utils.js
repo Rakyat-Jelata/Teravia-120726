f36288a253e5bf5b8ed3c5aba09cec96dc36c6a2
Rakyat-Jelata <rjelata012@gmail.com>
2026-07-12 17:52:10 +0700

    Delete showUpgradeModal function
    Remove showUpgradeModal function from utils.js.

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,11 +1,1 @@
-function showUpgradeModal() {
-    // Cari container modal di index.html atau buat dinamis
-    const modal = document.getElementById('membership-modal');
-    if (modal) {
-        modal.style.display = 'flex'; // Menggunakan flex untuk center overlay
-    } else {
-        alert("Upgrade ke Premium untuk memasang iklan!");
-        // Logika redirect atau show element bisa ditambahkan di sini
-    }
-}
 
